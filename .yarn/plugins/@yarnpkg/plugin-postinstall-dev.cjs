7bbd2b49c60d91bc5291d00715310b8a4541bf3f
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2022-07-16 22:09:19 -0700

    chore: update scaffold

--- .yarn/plugins/@yarnpkg/plugin-postinstall-dev.cjs
+++ .yarn/plugins/@yarnpkg/plugin-postinstall-dev.cjs
@@ -1,8 +1,9 @@
 /* eslint-disable */
+//prettier-ignore
 module.exports = {
 name: "@yarnpkg/plugin-postinstall-dev",
 factory: function (require) {
-var plugin;(()=>{"use strict";var e={d:(t,r)=>{for(var o in r)e.o(r,o)&&!e.o(t,o)&&Object.defineProperty(t,o,{enumerable:!0,get:r[o]})},o:(e,t)=>Object.prototype.hasOwnProperty.call(e,t),r:e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})}},t={};e.r(t),e.d(t,{default:()=>o});const r=require("child_process"),o={hooks:{async afterAllInstalled(e){await new Promise(t=>{const o=(0,r.spawn)("yarn",["run","postinstallDev"],{cwd:e.cwd});o.stdout.pipe(process.stdout),o.stderr.pipe(process.stderr),o.addListener("exit",()=>t())})}}};plugin=t})();
+var plugin=(()=>{var i=Object.create,s=Object.defineProperty;var n=Object.getOwnPropertyDescriptor;var d=Object.getOwnPropertyNames;var l=Object.getPrototypeOf,p=Object.prototype.hasOwnProperty;var u=t=>s(t,"__esModule",{value:!0});var f=t=>{if(typeof require!="undefined")return require(t);throw new Error('Dynamic require of "'+t+'" is not supported')};var g=(t,e)=>{for(var o in e)s(t,o,{get:e[o],enumerable:!0})},k=(t,e,o)=>{if(e&&typeof e=="object"||typeof e=="function")for(let r of d(e))!p.call(t,r)&&r!=="default"&&s(t,r,{get:()=>e[r],enumerable:!(o=n(e,r))||o.enumerable});return t},P=t=>k(u(s(t!=null?i(l(t)):{},"default",t&&t.__esModule&&"default"in t?{get:()=>t.default,enumerable:!0}:{value:t,enumerable:!0})),t);var x={};g(x,{default:()=>w});var c=P(f("@yarnpkg/core")),a="postinstallDev",h={hooks:{async afterAllInstalled(t){let e=t.topLevelWorkspace.anchoredLocator;if(await c.scriptUtils.hasPackageScript(e,a,{project:t})){let o=await c.scriptUtils.executePackageScript(e,a,[],{project:t,stdin:process.stdin,stdout:process.stdout,stderr:process.stderr});if(o!==0){let r=new Error(`${a} script failed with exit code ${o}`);throw r.stack=void 0,r}}}}},w=h;return x;})();
 return plugin;
 }
 };
